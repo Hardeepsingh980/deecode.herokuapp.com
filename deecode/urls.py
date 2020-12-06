581fadff27ef74a30aab02134cd6e209ea54522c
Hardeep Singh <hardeep0khalsa122@gmail.com>
2020-12-06 12:22:44 +0530

    removed js deps

--- deecode/urls.py
+++ deecode/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('about/',views.about, name='about')
 ]
