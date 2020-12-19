0f3985f302c6f698ce95a23e113fd866ff9c6d28
Hardeep Singh <hardeep0khalsa122@gmail.com>
2020-12-19 20:52:45 +0530

    added hukam api

--- deecode/views.py
+++ deecode/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+import json
 from . import code2
 from . import decorder
-
-
 
 
 def home(request):
@@ -10,15 +11,36 @@ def home(request):
         try:
             code_input = request.POST['codeinput']
             codeout = decorder.main_code(code_input)
-            return render(request, 'home.html',{'codeoutput':codeout})
+            return render(request, 'home.html', {'codeoutput': codeout})
         except:
             text_input = request.POST['textinput']
             textout = code2.main_code(text_input)
-            return render(request, 'home.html',{'textoutput':textout})
-            
-        print(code_input, text_input , 'Hello')
+            return render(request, 'home.html', {'textoutput': textout})
+
     else:
-        return render(request, 'home.html') 
+        return render(request, 'home.html')
+
+
+def sendHumkam(request):
+    data = {
+        "notification": {"body": "TODAY'S HUKAMNAMA FROM SRI DARBAR SAHIB, Sri Amritsar.", "title": "Hukamnama Sahib"},
+        "priority": "high",
+        "data": {
+            "click_action": "FLUTTER_NOTIFICATION_CLICK",
+            "hukam": 'true'
+        },
+        "to":"/topics/hukam",
+    }
+
+    headers = {
+      'content-type': 'application/json',
+      'Authorization': 'key=AAAAALKGjxw:APA91bGIQrURLECUCSy5dfFwDYers7dbK4WrqOL6ATPeMbiolIhOPuIUyCDAuMRQgnXctEOLL7tC9KspwJjaGOYByoq0fPbRSMar4ZgI76jmAk3uebv3QLO-3zbhvovh6Rz43cllJiTR'
+    }
+
+    res = requests.post('https://fcm.googleapis.com/fcm/send', data=json.dumps(data), headers=headers)
+
+    return JsonResponse(res.text, safe=False)
+
 
 def about(request):
     return render(request, 'about.html')
